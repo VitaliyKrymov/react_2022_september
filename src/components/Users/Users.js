5dd8bdbc168c3dd963642d1762e5b2d825f92bdc
VitaliiKrymov <krymvitaliy@gmail.com>
2022-09-19 00:00:06 +0300

    lesson2 21:14

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,21 +2,39 @@ import {useEffect, useState} from "react";
 
 import User from "../User/User";
 
-export  default function Users () {
-    let [users,setUsers]=useState([]);
+export default function Users() {
+    let [users, setUsers] = useState([]);
+    let [user, setUser] = useState(null);
+    // let [user,setUser]=useState({});
 
-    useEffect(()=>{
+    let lift = (obj) => {
+        console.log(obj);
+        setUser(obj)
+
+    }
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(value => value.json())
             .then(value => {
                 console.log(value);
                 setUsers(value);
             });
-    },[])
+    }, [])
 
     return (
         <div>
-            {users.map((user,index)=>(<User item={user} key={index}/>))}
+            <hr/>
+            {/*{user && <div>{JSON.stringify(user)}</div>}*/}
+            {/*{user? <div> user is </div> : <div> no user </div>}*/}
+            {/*{JSON.stringify(user)}*/}
+            <h3>{user?.username}</h3>
+            <hr/>
+            {users.map((user, index) => (<User
+                item={user}
+                key={index}
+                lift={lift}/>))};
+
         </div>
     );
 };
