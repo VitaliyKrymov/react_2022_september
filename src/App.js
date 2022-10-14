e4be1dcfa0024fea27f8bbf61d1e7c6f7f37b226
VitaliiKrymov <krymvitaliy@gmail.com>
2022-10-14 22:22:12 +0300

    Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import {useSelector} from "react-redux";
+
 function App() {
+    let state = useSelector(state => state);
+    console.log(state);
     return (
         <div>
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+let initState = {users: []};
+let store = createStore((state = initState, action) => {
+    console.log(state, action);
+    return state;
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
